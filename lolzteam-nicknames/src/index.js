c5955224a671699b2736857b8c6a15bb834bdff1
Viktor <59040542+VityaSchel@users.noreply.github.com>
2023-02-24 02:18:40 +0400

    it works!!!!

--- lolzteam-nicknames/src/index.js
+++ lolzteam-nicknames/src/index.js
@@ -1,10 +1,10 @@
 (() => {
-  const your_host_server = 'localhost:27109'
-  document.querySelectorAll('ol.memberList a.username span')
+  const your_host_server = 'http://localhost:27109'
+  Array.from(document.querySelectorAll('ol.memberList a.username span'))
     .forEach((username, i) => {
       const x = i % 5
       const y = ~~(i/5)
-      const filename = 'x'+x + '_' + 'y'+y
-      username.style.background = `url("${your_host_server + '/' + filename}") !important`
+      const filename = `x${x}_y${y}.webp`
+      username.style.setProperty('background', `url("${your_host_server + '/' + filename}")`, 'important')
     })
 })()
